dc33bfea9b0f4fade5e503ac049715d8375e29e1
TonyPoint <laridantony@laposte.net>
2018-12-03 15:57:42 +0100

    Ajout de NotSupportedException

---
+++ src/main/java/image/NotSupportedException.java
@@ -0,0 +1,10 @@
+package image;
+
+public class NotSupportedException extends RuntimeException{
+    String string;
+
+    public NotSupportedException(String string){
+        this.string = string;
+    }
+
+}
